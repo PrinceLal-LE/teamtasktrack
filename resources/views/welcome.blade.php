7ac0a57314906311000a3ce9c3a3d869bb936b0a
PrinceFromServer <cipetprincelal@gmail.com>
2026-01-05 00:47:46 +0530

    Removed the issue of showing default site

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,8 +24,7 @@
 
     <!-- Styles / Scripts -->
     @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
-    @vite(['resources/css/app.css','resources/css/homePageStyle.css', 'resources/js/app.js',
-    'resources/js/homePageJavaScriptCode.js'])
+    @vite(['resources/css/app.css','resources/js/app.js'])
     @endif
 </head>
 
